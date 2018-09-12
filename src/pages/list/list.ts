3af246da33fdcba82cadc951676b773126a40201
Jukka Kotamäki <jukka@MacBook-Pro.local>
2018-09-12 21:40:37 +0300

    pikku muutos

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -22,7 +22,7 @@ export class ListPage {
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Item ' + i,
-        note: 'Tää on item #' + i,
+        note: 'Tää on itemi #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
